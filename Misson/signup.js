be7d0bb802f778f60db6a96f39f6e419b51aaaf4
kssosoy <qws1566@naver.com>
2024-04-06 18:10:12 +0900

    수정중

--- Misson/signup.js
+++ Misson/signup.js
@@ -17,33 +17,89 @@ const passwdFail = document.querySelector('.fail-passwd');
 const passwd2=document.querySelector('#passwd2');
 const passwd2Success = document.querySelector('.success-passwd2');
 const passwd2Fail = document.querySelector('.fail-passwd2');
-const pattern=/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-let nameCheck = () => {
 
-    if (/[0-9]/.test(Name.value.trim())===true|| Name.value.trim() === '') {
-        nameFail.style.display = 'flex';
+
+const numPattern= /^\d+$/;
+const emailPattern=/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+const fourtweleve=  /^[a-z\d]{4,12}$/;
+const passwdpattern=/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/
+
+let nameCheck =()=>{
+    if(numPattern.test(Name.value)==true){
+        nameFail.innerText = '이름은 문자열이어야 합니다!';
         nameSuccess.style.display = 'none';
-    } else {
+        nameFail.style.display = 'flex';
+    }
+    else if(Name.value==''){
+        nameFail.innerText = '필수 입력 항목입니다!';
+        nameSuccess.style.display = 'none';
+        nameFail.style.display = 'flex';
+    }
+    else{
         nameSuccess.style.display = 'flex';
         nameFail.style.display = 'none';
     }
 }
-
-let emailCheck =() =>{
-    if(!/[a-zA-Z]/.test(email.value.trim())|| 
-    !pattern.test(email.value.trim())||
-    email.value.trim()===''){
-        emailFail.style.display = 'flex';
+let emailCheck =()=>{
+    if(numPattern.test(email.value)==true){
+        emailFail.innerText = '이메일은 문자열이어야 합니다!';
         emailSuccess.style.display = 'none';
-
+        emailFail.style.display = 'flex';
+    }
+    else if(email.value==''){
+        emailFail.innerText = '필수 입력 항목입니다!';
+        emailSuccess.style.display = 'none';
+        emailFail.style.display = 'flex';
+    }
+    else if(emailPattern.test(email.value)!==true){
+        emailFail.innerText = '이메일 양식을 맞춰주어야 합니다!';
+        emailSuccess.style.display = 'none';
+        emailFail.style.display = 'flex';
     }
     else{
         emailSuccess.style.display = 'flex';
         emailFail.style.display = 'none';
     }
 }
+let ageCheck =()=>{
+    if(numPattern.test(age.value)==false){
+        emailFail.innerText = '나이는 숫자입니다!';
+        emailSuccess.style.display = 'none';
+        emailFail.style.display = 'flex';
+    }
+    else if(email.value==''){
+        emailFail.innerText = '필수 입력 항목입니다!';
+        emailSuccess.style.display = 'none';
+        emailFail.style.display = 'flex';
+    }
+    else if(age.value<0){
+        emailFail.innerText = '나이는 음수가 될 수 없습니다';
+        emailSuccess.style.display = 'none';
+        emailFail.style.display = 'flex';
+    }
+    else if(age.value%1!==0){
+        emailFail.innerText = '나이는 소수가 될 수 없습니다 ';
+        emailSuccess.style.display = 'none';
+        emailFail.style.display = 'flex';
+    }
+    else if(age.value<=18){
+        emailFail.innerText = '19세 이상만 가입이 가능합니다. ';
+        emailSuccess.style.display = 'none';
+        emailFail.style.display = 'flex';
+    }
+    else{
+        emailSuccess.style.display = 'flex';
+        emailFail.style.display = 'none';
+    }
+}
+
+
+
 Name.addEventListener('blur', nameCheck);
 email.addEventListener('blur', emailCheck);
+age.addEventListener('blur',ageCheck);
+passwd. addEventListener('blur',passwdCheck);
+passwd2.addEventListener('blur',passwd2Check);
 let modalDisplay=()=>{
 
 }
